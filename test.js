71f26d5bb7b3d55122433ac304679a18b9bb3150
barisayyildiz <barisayyildiz.1799@gmail.com>
2020-08-16 14:28:15 +0300

    Veri çekildi

--- test.js
+++ test.js
@@ -1,28 +1,66 @@
-let cityName = "Istanbul";
+function processInput(cityName)
+{
 
-let urlForecast = "http://api.openweathermap.org/data/2.5/weather", keyForecast = "&APPID=e9b96acc72c0c1b9df8d81936190b1e3";
-urlForecast += "?q=" + cityName + keyForecast;
+	//forecast
+	let urlForecast = "http://api.openweathermap.org/data/2.5/weather", keyForecast = "&APPID=e9b96acc72c0c1b9df8d81936190b1e3";
+	urlForecast += "?q=" + cityName + keyForecast;
 
-//background image
-let urlImage = "https://api.unsplash.com/search/photos/?client_id=c2BDCKJ6tVIDatSz2zPZT90e3rJpU_wytclJbGZ4zMo&page=1&per_page=1&query=";
+	//background image
+	let urlImage = "https://api.unsplash.com/search/photos/?client_id=", keyImage = "c2BDCKJ6tVIDatSz2zPZT90e3rJpU_wytclJbGZ4zMo";
+	urlImage += keyImage + "&page=1&per_page=1&query=" + cityName;
 
-urlImage += cityName;
+	//fetch from OpenWeather API
+	fetch(urlForecast)
+	.then(response => response.json())
+	.then(data => {
+
+		console.log(data.cod);
+		if(data.cod == 200)
+		{
+			console.log(data);
+
+			//datas we will use
+			let celcius = (data.main.temp - 273).toFixed(1);
+			let countryId = data.sys.country;
+			let iconId = data.weather[0].id;
+			let description = data.weather[0].description;
+
+			let mes = document.querySelector("p");
+			mes.innerText = celcius + countryId + iconId + description;
 
 
-fetch(urlForecast)
-.then(response => response.json())
-.then(data => {
 
-	if(data.cod == 200)
-	{
-		console.log(data);
+		}else if(data.message == "Nothing to geocode")
+		{
+			//do nothing
+
+		}else if(data.cod == 404)
+		{
+			//error message		city not found
+			let mes = document.querySelector("p");
+			mes.innerText = data.message;
+			console.log(data);
+		}
+	})
+
+}
 
 
+function getInput()
+{
+	const button = document.querySelector("button");
+	const input = document.querySelector("input");
 
-	}else if(data.cod == 404)
-	{
-		//hata kodu
-		console.log(data.message);
-	}
+	button.addEventListener("click", processInput(input.value));
 
-})
+	input.addEventListener("keyup", (event) => {
+		//pressed enter
+		if(event.keyCode == 13)
+		{
+			return processInput(input.value);
+		}
+	})
+
+}
+
+getInput();
